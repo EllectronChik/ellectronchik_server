c4e7b8e79dcaa9c1d58e3e451c9fa3240fb359d2
EllectronChik <electronchik04@mail.ru>
2024-04-03 18:34:47 +0300

    create, findOne, findAll & remove methods

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { User, UserDocument } from './schema/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from './dto/CreateUserDto';
+import { RolesService } from 'src/roles/roles.service';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private RolesService: RolesService,
+  ) {}
+
+  async create(dto: CreateUserDto): Promise<UserDocument> {
+    const userRole = await this.RolesService.findOne('user');
+    const user = await this.userModel.create({ ...dto, rating: 0, role: userRole._id });
+    if (userRole.users) {
+      userRole.users.push(user);
+    } else {
+      userRole.users = [user];
+    }
+    console.log(userRole.users);
+    
+    await userRole.save();
+    return user;
+  }
+
+  async findOne(username: string): Promise<UserDocument> {
+    return this.userModel.findOne({ name: username });
+  }
+
+  async findAll(): Promise<UserDocument[]> {
+    return this.userModel.find();
+  }
+
+  async remove(id: string): Promise<boolean> {
+    const user = await this.userModel.findById(id);
+    await this.RolesService.removeUserFromRole(id, user.role.name);
+    return !!(await user.deleteOne());
+  }
+}
