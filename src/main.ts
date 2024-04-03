2251d9ca7590ff47c8cbfa63c00257a12f1b5cff
EllectronChik <electronchik04@mail.ru>
2024-04-03 18:21:50 +0300

    added helmet & rm dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
-
-// Load environment variables
-config();
+import helmet from 'helmet';
 
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);
+    app.use(helmet());
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.error(error);
