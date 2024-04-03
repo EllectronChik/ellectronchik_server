6bea38fa32694716540b7d2a862af4afb2d74c28
EllectronChik <electronchik04@mail.ru>
2024-04-03 19:51:12 +0300

    imported userModel

--- src/database-initializer/database-initializer.module.ts
+++ src/database-initializer/database-initializer.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseInitializerService } from './database-initializer.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Role, RoleSchema } from 'src/roles/schema/role.schema';
+import { User, UserSchema } from 'src/users/schema/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [DatabaseInitializerService],
 })
