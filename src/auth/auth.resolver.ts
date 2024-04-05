cfec8f969d2c17c9aa8ce259564b094db59bb98b
EllectronChik <electronchik04@mail.ru>
2024-04-05 16:55:13 +0300

    added refreshToken cookie to response

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,8 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { User } from 'src/users/schema/user.schema';
 import { AuthService } from './auth.service';
 import { AuthPayloadObject } from 'src/objects/authPayload.object';
+import { Response } from 'express';
 
 @Resolver((of) => User)
 export class AuthResolver {
@@ -11,12 +12,38 @@ export class AuthResolver {
   async register(
     @Args('name') name: string,
     @Args('password') password: string,
+    @Context('res') res: Response,
   ): Promise<{ accessToken: string }> {
-    return this.authService.signUp({ name, password });
+    const { accessToken, refreshToken } = await this.authService.signUp({
+      name,
+      password,
+    });
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+    });
+
+    return { accessToken };
   }
 
   @Mutation((returns) => AuthPayloadObject)
-  async login(@Args('name') name: string, @Args('password') password: string) {
-    return this.authService.signIn({ name, password });
+  async login(
+    @Args('name') name: string,
+    @Args('password') password: string,
+    @Context('res') res: Response,
+  ) {
+    const { accessToken, refreshToken } = await this.authService.signIn({
+      name,
+      password,
+    });
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+    });
+    return { accessToken, refreshToken };
   }
 }
