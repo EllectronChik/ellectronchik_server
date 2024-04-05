7559094d4d799ce9f99b2785e1baa5f6d1f99ee8
EllectronChik <electronchik04@mail.ru>
2024-04-05 17:37:10 +0300

    added user role context

--- src/roles/roles.resolver.ts
+++ src/roles/roles.resolver.ts
@@ -1,21 +1,26 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Query, Resolver } from '@nestjs/graphql';
 import { RolesService } from './roles.service';
 import { RoleObject, RolesObject } from 'src/objects/role.object';
 import { RoleDocument } from './schema/role.schema';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Resolver()
 export class RolesResolver {
   constructor(private readonly RolesService: RolesService) {}
 
+  @UseGuards(AuthGuard)
   @Query(() => [RolesObject])
-  async findAllRoles(): Promise<RoleDocument[]> {
-    const roles = await this.RolesService.findAll();
-    return roles
+  async findAllRoles(
+    @Context('req') req: { user: { role: string } },
+  ): Promise<RoleDocument[]> {
+    const roles = await this.RolesService.findAllByRole(req.user.role);
+    return roles;
   }
 
   @Query(() => RoleObject)
   async findOneRole(@Args('name') name: string): Promise<RoleDocument> {
     const role = (await this.RolesService.findOne(name)).populate('users');
-    return role
+    return role;
   }
 }
