e61f7c41daaef337f6b95e5c4e39e469ea68a458
EllectronChik <electronchik04@mail.ru>
2024-04-05 16:58:34 +0300

    added refresh token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,15 +4,19 @@ import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from '../users/dto/CreateUserDto';
 import { JwtService } from '@nestjs/jwt';
 import { UserDocument } from 'src/users/schema/user.schema';
+import { RefreshService } from '../refresh/refresh.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private RefreshService: RefreshService,
   ) {}
 
-  async signUp(userDto: CreateUserDto): Promise<{ accessToken: string }> {
+  async signUp(
+    userDto: CreateUserDto,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
     const candidate = await this.usersService.findOne(userDto.name);
     if (candidate) {
       throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
@@ -22,17 +26,23 @@ export class AuthService {
       ...userDto,
       password: passHash,
     });
+    const { refreshToken } = await this.RefreshService.create(user._id);
+    const { accessToken } = await this.generateToken(user);
 
-    return this.generateToken(user);
+    return { accessToken, refreshToken };
   }
 
-  async signIn(userDto: CreateUserDto): Promise<{ accessToken: string }> {
+  async signIn(
+    userDto: CreateUserDto,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
     const user = await this.usersService.findOne(userDto.name);
-    
-    const isMatch = await bcrypt.compare(userDto.password, user.password);    
+
+    const isMatch = await bcrypt.compare(userDto.password, user.password);
 
     if (user && isMatch) {
-      return this.generateToken(user);
+      const { refreshToken } = await this.RefreshService.create(user._id);
+      const { accessToken } = await this.generateToken(user);
+      return { accessToken, refreshToken };
     }
 
     throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
@@ -41,7 +51,12 @@ export class AuthService {
   private async generateToken(
     user: UserDocument,
   ): Promise<{ accessToken: string }> {
-    const payload = { username: user.name, sub: user._id, role: user.role, rating: user.rating };
+    const payload = {
+      username: user.name,
+      sub: user._id,
+      role: user.role,
+      rating: user.rating,
+    };
 
     return {
       accessToken: await this.jwtService.signAsync(payload),
