4ea93ec0d483f3885261e374efdb9e09845bf39d
EllectronChik <electronchik04@mail.ru>
2024-04-11 16:03:49 +0300

    created schema

---
+++ src/tags/schema/tag.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { User } from 'src/users/schema/user.schema';
+
+export type TagDocument = mongoose.Document;
+
+@Schema()
+export class Tag {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  color: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  userId: User;
+}
+
+export const TagSchema = SchemaFactory.createForClass(Tag);
