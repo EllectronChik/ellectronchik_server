5177c6158b89998bed2eff9757e1c5a7792eb52d
EllectronChik <electronchik04@mail.ru>
2024-04-11 16:05:18 +0300

    added findAll, findPaginated && findOne methods

--- src/diary-notes/diary-notes.service.ts
+++ src/diary-notes/diary-notes.service.ts
@@ -4,4 +4,28 @@ import { DiaryNote, DiaryNoteDocument } from './schema/diaryNote.schema';
 import { Model } from 'mongoose';
 
 @Injectable()
-export class DiaryNotesService {}
+export class DiaryNotesService {
+  constructor(
+    @InjectModel(DiaryNote.name) private diaryNoteModel: Model<DiaryNoteDocument>,
+  ) {}
+
+  async findAll(): Promise<DiaryNoteDocument[]> {
+    return await this.diaryNoteModel.find();
+  }
+
+  async findPaginated(
+    page: number,
+    limit: number,
+  ): Promise<DiaryNoteDocument[]> {
+    return await this.diaryNoteModel
+      .find()
+      .skip((page - 1) * limit)
+      .limit(limit);
+  }
+
+  async findOne(id: string): Promise<DiaryNoteDocument> {
+    return await this.diaryNoteModel.findById(id);
+  }
+
+  // async create(dto: any): Promise<DiaryNoteDocument> {}
+}
