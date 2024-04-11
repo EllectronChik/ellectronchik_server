cd66e7c4e9d0bfd6786928329568de48cbbbd4b3
EllectronChik <electronchik04@mail.ru>
2024-04-11 16:06:44 +0300

    added findAll, findOne, findTagsByUser && createTag endpoints

--- src/tags/tags.resolver.ts
+++ src/tags/tags.resolver.ts
@@ -10,5 +10,39 @@ import { Roles } from 'src/auth/roles.decorator';
 
 @Resolver(() => TagObject)
 export class TagsResolver {
+  constructor(private readonly tagsService: TagsService) {}
 
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
+  @Query(() => [TagObject], { name: 'findAllTags' })
+  async findAll(): Promise<TagDocument[]> {
+    return this.tagsService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(() => TagObject, { name: 'findUserTagById' })
+  async findOne(
+    @Args('id') id: string,
+    @Context('req') { user: { sub: userId } }: IReqUserContext,
+  ): Promise<TagDocument> {
+    return this.tagsService.findOne(id, userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(() => [TagObject], { name: 'findTagsByUser' })
+  async findTagsByUser(
+    @Context('req') { user: { sub: userId } }: IReqUserContext,
+  ): Promise<TagDocument[]> {
+    return this.tagsService.findTagsByUser(userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => TagObject, { name: 'createTag' })
+  async createTag(
+    @Args('name') name: string,
+    @Args('color') color: string,
+    @Context('req') { user: { sub: userId } }: IReqUserContext,
+  ): Promise<TagDocument> {
+    return this.tagsService.create({ name, color, userId });
+  }
 }
