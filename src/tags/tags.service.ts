c6264dfd598f6b2501594074c90ccc9485796683
EllectronChik <electronchik04@mail.ru>
2024-04-11 16:06:11 +0300

    added findOne, findAll, findTagsByUser && create method

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -6,5 +6,26 @@ import { createTagDto } from './dto/createTagDto';
 
 @Injectable()
 export class TagsService {
+  constructor(
+    @InjectModel(Tag.name) private readonly tagModel: Model<TagDocument>,
+  ) {}
 
+  async findAll(): Promise<TagDocument[]> {
+    return this.tagModel.find();
+  }
+
+  async findOne(id: string, userId: string): Promise<TagDocument> {
+    if (userId) {
+      return this.tagModel.findOne({ _id: id, userId });
+    }
+    return null;
+  }
+
+  async findTagsByUser(userId: string): Promise<TagDocument[]> {
+    return this.tagModel.find({ userId });
+  }
+
+  async create(dto: createTagDto): Promise<TagDocument> {
+    return this.tagModel.create(dto);
+  }
 }
