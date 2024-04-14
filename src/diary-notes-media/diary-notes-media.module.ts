4dc2a4a9c2c96b8347d7e0b6360d0e0c0526fb02
EllectronChik <electronchik04@mail.ru>
2024-04-14 20:52:38 +0300

    added DiaryNote model + DiaryNotesMediaController

--- src/diary-notes-media/diary-notes-media.module.ts
+++ src/diary-notes-media/diary-notes-media.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { DiaryNotesMediaService } from './diary-notes-media.service';
-import { DiaryNotesMediaResolver } from './diary-notes-media.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DiaryNoteMedia, DiaryNoteMediaSchema } from './schema/diary-notes-media.schema';
+import {
+  DiaryNoteMedia,
+  DiaryNoteMediaSchema,
+} from './schema/diary-notes-media.schema';
+import { DiaryNotesMediaController } from './diary-notes-media.controller';
+import {
+  DiaryNote,
+  DiaryNoteSchema,
+} from 'src/diary-notes/schema/diaryNote.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: DiaryNoteMedia.name, schema: DiaryNoteMediaSchema },
     ]),
+    MongooseModule.forFeature([
+      { name: DiaryNote.name, schema: DiaryNoteSchema },
+    ]),
   ],
-  providers: [DiaryNotesMediaService, DiaryNotesMediaResolver]
+  providers: [DiaryNotesMediaService],
+  controllers: [DiaryNotesMediaController],
 })
 export class DiaryNotesMediaModule {}
