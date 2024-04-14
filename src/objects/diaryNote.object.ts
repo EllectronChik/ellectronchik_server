eb20e2133ab88ae1747149a1eb1015d0f43e8bce
EllectronChik <electronchik04@mail.ru>
2024-04-14 20:54:04 +0300

    created object

---
+++ src/objects/diaryNote.object.ts
@@ -0,0 +1,28 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class DiaryNoteObject {
+  @Field()
+  _id: string;
+
+  @Field()
+  encryptedTitle: string;
+
+  @Field()
+  encryptedText: string;
+
+  @Field(() => [String], { nullable: true })
+  encryptedMedia: string[];
+
+  @Field()
+  createdAt: Date;
+
+  @Field({ nullable: true })
+  updatedAt: Date | null;
+
+  @Field(() => [String], { nullable: true })
+  tags: string[];
+
+  @Field()
+  userId: string;
+}
