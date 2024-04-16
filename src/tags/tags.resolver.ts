057c3a1ca1a867a4ebfbbef2bfaa693ba1fc3952
EllectronChik <electronchik04@mail.ru>
2024-04-16 16:50:00 +0300

    TagObject => Tag.entity && added createTagInput

--- src/tags/tags.resolver.ts
+++ src/tags/tags.resolver.ts
@@ -1,20 +1,21 @@
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { TagsService } from './tags.service';
 import { TagDocument } from './schema/tag.schema';
-import { TagObject } from 'src/objects/tag.object';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import IReqUserContext from 'src/models/IReqUserContext';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/roles.decorator';
+import { Tag } from './entities/tag.entity';
+import { createTagInput } from './dto/create-tag.input';
 
-@Resolver(() => TagObject)
+@Resolver(() => Tag)
 export class TagsResolver {
   constructor(private readonly tagsService: TagsService) {}
 
   @UseGuards(RolesGuard)
   @Roles('ADMIN')
-  @Query(() => [TagObject], {
+  @Query(() => [Tag], {
     name: 'findAllTags',
     description: 'Find all tags, admin only',
   })
@@ -23,7 +24,7 @@ export class TagsResolver {
   }
 
   @UseGuards(AuthGuard)
-  @Query(() => TagObject, {
+  @Query(() => Tag, {
     name: 'findUserTagById',
     description: 'Find tag associated with user by its id, auth required',
   })
@@ -35,7 +36,7 @@ export class TagsResolver {
   }
 
   @UseGuards(AuthGuard)
-  @Query(() => [TagObject], {
+  @Query(() => [Tag], {
     name: 'findTagsByUser',
     description: 'Find all tags associated with user by its id, auth required',
   })
@@ -46,16 +47,15 @@ export class TagsResolver {
   }
 
   @UseGuards(AuthGuard)
-  @Mutation(() => TagObject, {
+  @Mutation(() => Tag, {
     name: 'createTag',
     description:
       'Create new tag wich will be accessible only for creator, auth required',
   })
   async createTag(
-    @Args('name') name: string,
-    @Args('color') color: string,
+    @Args('createTagInput') createTagInput: createTagInput,
     @Context('req') { user: { sub: userId } }: IReqUserContext,
   ): Promise<TagDocument> {
-    return this.tagsService.create({ name, color, userId });
+    return this.tagsService.create(createTagInput, userId);
   }
 }
