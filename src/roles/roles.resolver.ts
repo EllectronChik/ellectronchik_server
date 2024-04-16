f1bef91043c08384f64d1591e9834d6dafbaabab
EllectronChik <electronchik04@mail.ru>
2024-04-16 16:49:27 +0300

    RoleObject => Role.entity && RolesObject => Roles.entity

--- src/roles/roles.resolver.ts
+++ src/roles/roles.resolver.ts
@@ -1,11 +1,13 @@
 import { Args, Context, Query, Resolver } from '@nestjs/graphql';
 import { RolesService } from './roles.service';
-import { RoleObject, RolesObject } from 'src/objects/role.object';
 import { RoleDocument } from './schema/role.schema';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/roles.decorator';
+import { Role } from './entities/role.entity';
+import { Roles as RolesObject } from './entities/roles.entity';
+
 
 @Resolver()
 export class RolesResolver {
@@ -25,7 +27,7 @@ export class RolesResolver {
 
   @UseGuards(RolesGuard)
   @Roles('ADMIN')
-  @Query(() => RoleObject, {
+  @Query(() => Role, {
     name: 'findOneRole',
     description: 'Get role by name, admin only',
   })
