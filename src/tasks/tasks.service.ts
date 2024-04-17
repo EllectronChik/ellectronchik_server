b50bfacb5906a909400c4f53fbedc929f948ed6d
EllectronChik <electronchik04@mail.ru>
2024-04-17 15:48:30 +0300

    added user rating change logic

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,12 +5,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Task, TaskDocument } from './schema/task.schema';
 import { Model } from 'mongoose';
 import { Subtask, SubtaskDocument } from 'src/subtask/schema/subtask.schema';
+import { User, UserDocument } from 'src/users/schema/user.schema';
+import { Interval } from '@nestjs/schedule';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectModel(Task.name) private taskModel: Model<TaskDocument>,
     @InjectModel(Subtask.name) private subtaskModel: Model<SubtaskDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
 
   create(createTaskInput: CreateTaskInput, userId: string) {
@@ -28,12 +31,34 @@ export class TasksService {
       .populate('subtasks');
   }
 
-  update(id: string, userId: string, updateTaskInput: UpdateTaskInput) {
-    return this.taskModel.findOneAndUpdate(
+  async update(id: string, userId: string, updateTaskInput: UpdateTaskInput) {
+    const task = await this.taskModel.findOneAndUpdate(
       { _id: id, userId },
       updateTaskInput,
       { new: true },
     );
+    if (updateTaskInput.completed !== undefined) {
+      if (updateTaskInput.completed) {
+        return this.userModel.findOne({ _id: userId }).then((user) => {
+          let points = task.points;
+          if (task.isOverdue) {
+            points /= 2;
+          }
+          user.rating += points;
+          user.save();
+        });
+      } else {
+        return this.userModel.findOne({ _id: userId }).then((user) => {
+          let points = task.points;
+          if (task.isOverdue) {
+            points /= 2;
+          }
+          user.rating -= points;
+          user.save();
+        });
+      }
+    }
+    return task;
   }
 
   async remove(id: string, userId: string) {
@@ -48,6 +73,32 @@ export class TasksService {
     } catch (error) {
       console.error(error);
       return false;
+    }
+  }
+
+  async scheduleTaskCompletion(id: string, userId: string, date: Date) {
+    const currentDate = new Date();
+    const task = await this.taskModel.findOne({ _id: id, userId });
+    if (task && currentDate > date && !task.completed) {
+      task.isOverdue = true;
+      task.save();
+      return true;
+    }
+  }
+
+  @Interval(1000 * 60)
+  async checkOverdueTasks() {
+    const currentDate = new Date();
+
+    const tasks = await this.taskModel.find({
+      dueDate: { $lt: currentDate },
+      isOverdue: false,
+      completed: false,
+    });
+
+    for (const task of tasks) {
+      task.isOverdue = true;
+      task.save();
     }
   }
 }
