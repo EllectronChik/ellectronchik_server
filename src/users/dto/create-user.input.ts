04ab672ce7ce1a3aabdc439db28a5a2c5e1269a3
EllectronChik <electronchik04@mail.ru>
2024-04-21 18:41:40 +0300

    added validation rules

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,10 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString, IsStrongPassword, Length } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @Length(3, 20)
+  @IsString()
   readonly name: string;
 
   @Field()
+  @IsNotEmpty()
+  @Length(6, 20)
+  @IsStrongPassword()
   readonly password: string;
 }
