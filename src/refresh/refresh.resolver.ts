02d01a2f149bc1cc94ebf18fe8e2e809d1031a23
EllectronChik <electronchik04@mail.ru>
2024-04-27 19:03:34 +0300

    added data to return body && fixed split bug

--- src/refresh/refresh.resolver.ts
+++ src/refresh/refresh.resolver.ts
@@ -3,12 +3,13 @@ import { Context, Resolver, Query } from '@nestjs/graphql';
 import { GraphQLError } from 'graphql';
 import { RefreshService } from './refresh.service';
 import { Response } from 'express';
+import { Refresh } from './entities/refresh.entity';
 
 @Resolver()
 export class RefreshResolver {
   constructor(private readonly refreshService: RefreshService) {}
 
-  @Query(() => Boolean, {
+  @Query(() => Refresh, {
     name: 'revalidateToken',
     description:
       'Revalidate token using refresh token, return access token and new refresh token',
@@ -16,14 +17,17 @@ export class RefreshResolver {
   async revalidateToken(
     @Context('req') req: { headers: { cookie: string } },
     @Context('res') res: Response,
-  ): Promise<boolean> {
+  ): Promise<{ accessToken: string; refreshToken: string }> {
     let oldrefreshTokenCookie: string;
     let userId: string;
     let oldrefreshToken: string;
     try {
       oldrefreshTokenCookie = req.headers.cookie
         .split('refresh-token=')[1]
-        .split(';')[0];
+      if (oldrefreshTokenCookie && oldrefreshTokenCookie.includes(';')) {
+        oldrefreshTokenCookie = oldrefreshTokenCookie.split(';')[0];
+      }
+
       userId = oldrefreshTokenCookie.split('__')[0];
       oldrefreshToken = oldrefreshTokenCookie.split('__')[1];
     } catch (e) {
@@ -36,22 +40,26 @@ export class RefreshResolver {
         extensions: { code: HttpStatus.UNAUTHORIZED },
       });
     }
+
     const { accessToken, refreshToken } = await this.refreshService.reValidate(
       userId,
       oldrefreshToken,
     );
     res.cookie('refresh-token', `${userId}__${refreshToken}`, {
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 30,
     });
     res.cookie('x-access-token', accessToken, {
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 15,
     });
-    return true;
+    return {
+      accessToken,
+      refreshToken: `${userId}__${refreshToken}`,
+    };
   }
 }
