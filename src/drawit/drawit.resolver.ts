3d0e2098ff88f41e146096259db33a4b25d872f3
ellectronchik <electronchik04@mail.ru>
2024-07-08 18:09:32 +0300

    added joinGame mutation && getCommonData subscription

--- src/drawit/drawit.resolver.ts
+++ src/drawit/drawit.resolver.ts
@@ -10,11 +10,14 @@ import { DrawitService } from './drawit.service';
 import { Drawit } from './entities/drawit.entity';
 import { CreateDrawitInput } from './dto/create-drawit.input';
 import { GameSub } from './entities/gameSub.entity';
-// import { UpdateDrawitInput } from './dto/update-drawit.input';
 import { GraphQLError } from 'graphql';
 import { CreatedGame } from './entities/createdGame.entity';
 import { StartGameInput } from './dto/start-game.input';
 import { SubscriptionService } from './subscription.service';
+import { JoinGame } from './entities/joinGame.entity';
+import { JoinGameInput } from './dto/join-game.input';
+import { CommonGameData } from './entities/commonGameData.entity';
+import { AddChatMessageInput } from './dto/add-chat-message.input';
 
 @Resolver(() => Drawit)
 export class DrawitResolver {
@@ -23,34 +26,22 @@ export class DrawitResolver {
     private readonly subscriptionService: SubscriptionService,
   ) {}
 
-  @Mutation(() => Drawit, { name: 'CreateDrawItPack' })
+  @Mutation(() => Drawit, { name: 'createDrawItPack' })
   createDrawit(
     @Args('createDrawitInput') createDrawitInput: CreateDrawitInput,
   ) {
     return this.drawitService.create(createDrawitInput);
   }
 
-  @Query(() => [Drawit], { name: 'FindAllDrawItPacks' })
+  @Query(() => [Drawit], { name: 'findAllDrawItPacks' })
   findAll() {
     return this.drawitService.findAll();
   }
 
-  @Query(() => Drawit, { name: 'FindOneDrawItPack' })
+  @Query(() => Drawit, { name: 'findOneDrawItPack' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.drawitService.findOne(id);
   }
-
-  // @Mutation(() => Drawit, { name: 'UpdateDrawItPack' })
-  // updateDrawit(
-  //   @Args('updateDrawitInput') updateDrawitInput: UpdateDrawitInput,
-  // ) {
-  //   return this.drawitService.update(updateDrawitInput.id, updateDrawitInput);
-  // }
-
-  // @Mutation(() => Drawit, { name: 'RemoveDrawItPack' })
-  // removeDrawit(@Args('id', { type: () => Int }) id: number) {
-  //   return this.drawitService.remove(id);
-  // }
 
   @Subscription(() => [GameSub], { name: 'getGames' })
   getGames() {
@@ -58,6 +49,13 @@ export class DrawitResolver {
       this.drawitService.getGames();
     }, 0);
     return this.subscriptionService.getPubSub.asyncIterator('getGames');
+  }
+
+  @Subscription(() => CommonGameData, { name: 'commonData' })
+  getCommonData(@Args('gameId') gameId: string) {
+    return this.subscriptionService.getPubSub.asyncIterator(
+      `${gameId}_commonData`,
+    );
   }
 
   @Mutation(() => CreatedGame, { name: 'startGame' })
@@ -69,5 +67,28 @@ export class DrawitResolver {
         extensions: { code: 'BAD_USER_INPUT' },
       });
     }
+  }
+
+  @Mutation(() => JoinGame, { name: 'joinGame' })
+  async joinGame(@Args('joinGameInput') joinGameInput: JoinGameInput) {
+    try {
+      return this.drawitService.joinGame(joinGameInput);
+    } catch (e) {
+      throw new GraphQLError(e.message, {
+        extensions: { code: 'BAD_USER_INPUT' },
+      });
+    }
+  }
+
+  @Query(() => Boolean, { name: 'isGameExist' })
+  isGameExist(@Args('gameId') gameId: string) {
+    return this.drawitService.isGameExist(gameId);
+  }
+
+  @Mutation(() => Boolean, { name: 'addChatMessage' })
+  addChatMessage(
+    @Args('addChatMessageInput') addChatMessageInput: AddChatMessageInput,
+  ) {
+    return this.drawitService.addChatMessage(addChatMessageInput);
   }
 }
